271c2ac4d4dc05ffbddbfd48c7142da2c292c8c5
mohrezkhorasani <moh.khorasani@ut.ac.ir>
2026-02-16 13:42:15 +0330

    first commit

--- src/Component/Home/Product/GridMainProductCard.jsx
+++ src/Component/Home/Product/GridMainProductCard.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
-
+import img from "@assets/ax/ups/2025/02/back1.png"
 
 
 export default function GridMainProductCard({title,img,payPrice,oldPrice}) {
 
     return (
         <div className='flex flex-col items-center'>
-            <img src=''/>
+            <img src={img}/>
+            <p>faefaef</p>
         </div>  
     )
-}
+} 
